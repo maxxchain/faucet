b8d7d952150d1e749d58b93f37379e76d38a12e5
bigchiano <bigchiano@protonmail.com>
2023-05-01 23:35:13 +0300

    changed names

--- index.js
+++ index.js
@@ -32,13 +32,13 @@ app.configureWeb3(config)
 	require('./src/controllers/index')(app)
 
 	app.get('/', function(request, response) {
-	  response.send('Sokol POA Network faucet')
+	  response.send('MaxxChain Test Network faucet')
 	});
 
 	app.set('port', (process.env.PORT || 5000))
 
 	app.listen(app.get('port'), function () {
-	    console.log('Sokol testnet POA Network faucet is running on port', app.get('port'))
+	    console.log('MaxxChain Test Network faucet is running on port', app.get('port'))
 	})
 })
 .catch(error => {
