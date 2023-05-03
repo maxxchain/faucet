2f85e36a2f889f5a8590b9a71913773377ca25d8
bigchiano <bigchiano@protonmail.com>
2023-05-03 17:37:09 +0300

    add network helper

--- public/assets/javascripts/application/index.js
+++ public/assets/javascripts/application/index.js
@@ -1,15 +1,15 @@
-$(function() {
+$(function () {
 	var loader = $(".loading-container");
-	$( "#faucetForm" ).submit(function( e ) {
+	$("#faucetForm").submit(function (e) {
 		e.preventDefault();
-    	$this = $(this);
+		$this = $(this);
 		loader.removeClass("hidden");
 		var receiver = $("#receiver").val();
 		$.ajax({
-		  	url:"/",
-		  	type:"POST",
-		  	data: $this.serialize()
-		}).done(function(data) {
+			url: "/",
+			type: "POST",
+			data: $this.serialize()
+		}).done(function (data) {
 			grecaptcha.reset();
 			if (!data.success) {
 				loader.addClass("hidden");
@@ -22,13 +22,34 @@ $(function() {
 			$("#receiver").val('');
 			loader.addClass("hidden");
 			swal("Success",
-			  `0.1 PWR has been successfully transferred to <a href="https://testexplorer.maxxchain.org/tx/${data.success.txHash}" target="blank">${receiver}</a>`,
-			  "success"
+				`0.1 PWR has been successfully transferred to <a href="https://testexplorer.maxxchain.org/tx/${data.success.txHash}" target="blank">${receiver}</a>`,
+				"success"
 			);
-		}).fail(function(err) {
+		}).fail(function (err) {
 			grecaptcha.reset();
 			console.log(err);
 			loader.addClass("hidden");
 		});
 	});
+	
+	$("#addNetwork").click(function (e) {
+		window.ethereum.request({
+			method: 'wallet_addEthereumChain',
+			params: [{
+				chainId: '0x3FF',
+				chainName: 'Test MaxxChain',
+				nativeCurrency: {
+					name: 'PWRT Coin',
+					symbol: 'PWRT',
+					decimals: 18
+				},
+				rpcUrls: ['https://testrpc.maxxchain.org'],
+				blockExplorerUrls: ['https://testexplorer.maxxchain.org']
+			}]
+		})
+			.catch((error) => {
+				console.log(error)
+			})
+	})
+
 });
